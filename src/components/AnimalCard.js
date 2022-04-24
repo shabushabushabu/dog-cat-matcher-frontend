d8e89caacfd7f59f06f5469452817ed5833608b7
shabushabushabu <enigma14389@gmail.com>
2022-04-24 16:02:53 +0700

    change animal card styles and layouts

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,35 +1,96 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import PetsIcon from '@mui/icons-material/Pets';
+import { styled } from '@mui/material/styles';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import ShabuImage from "../figures/SHABU_profile3.jpg"
+const theme = createTheme({
+    palette: {
+        background: {
+            default: "#ffebee"
+        },
+        primary: {
+            main: "#ce93d8",
+        },
+        secondary: {
+            main: '#84c887',
+        },
+        error: {
+            main: "#f6a5c0"
+        },
+        warning: {
+            main: "#ffbba7"
+        }
+    }
+});
 
-function AnimalCard() {
+const BootstrapTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+    [`& .${tooltipClasses.arrow}`]: {
+        color: theme.palette.common.black,
+    },
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: theme.palette.common.black,
+    },
+}));
+
+function AnimalCard(props) {
+
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                alt="shabu"
-                height="140"
-                image={ShabuImage}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Shabu the Corgi
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lovely corgi dog
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Adopt</Button>
-            </CardActions>
-        </Card>
+        <ThemeProvider theme={theme}>
+            <Card
+                sx={{ width: 250, m: 3, textAlign: "left" }}>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{ bgcolor: "primary.main" }} aria-label="recipe">
+                            {props.name.charAt(0)}
+                        </Avatar>
+                    }
+
+                    title={props.name}
+                //   subheader="September 14, 2016"
+
+
+                />
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image={props.photoUrl}
+                    alt="animal-image"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.primary">
+                        {props.description}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {props.tags.map(tag => "#" + tag + " ")}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <BootstrapTooltip title="Adopt Me!">
+                        <IconButton aria-label="adopt" size="medium" color="primary">
+                            <PetsIcon />
+                        </IconButton>
+                    </BootstrapTooltip>
+                </CardActions>
+            </Card>
+        </ThemeProvider>
     );
 }
 
 export default AnimalCard;
+
+
+//TODO petICON
+// 1.  State
+// 2. Color
+// 3. Positiion
--- src/pages/AdoptAnimals.js
+++ src/pages/AdoptAnimals.js
@@ -1,55 +1,100 @@
-import react, {Component, useState, useEffect} from 'react';
+import react, { Component, useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import AnimalCard from "../components/AnimalCard"
 
+import ShabuImage from "../figures/SHABU_profile3.jpg"
 
 const theme = createTheme({
-    palette: {
-      background: {
-        default: "#ffebee"
-        },
-      primary: {
-        main: "#ce93d8",
-      },
-      secondary: {
-        main: '#84c887',
-      },
-      error: {
-          main: "#f6a5c0"
-      },
-      warning: {
-          main: "#ffbba7"
-      }
+  palette: {
+    background: {
+      default: "#ffebee"
+    },
+    primary: {
+      main: "#ce93d8",
+    },
+    secondary: {
+      main: '#84c887',
+    },
+    error: {
+      main: "#f6a5c0"
+    },
+    warning: {
+      main: "#ffbba7"
     }
-  });
+  }
+});
+
+const animalData = [
+  { name: "Shabu1", description: "A very lovely dog who loves salmon and beef. He poos 3 times a day and is scared of cats", photoUrls: [ShabuImage], tags: ["Corgi", "Dog", "Lovely", "Salmon", "Beef"] },
+  { name: "Zhabu2", description: "A fat dog with no tail. He barks at almost everything", photoUrls: [ShabuImage], tags: ["Corgi", "Dog", "Peach"] },
+  { name: "Shabu3", description: "A corgi dog", photoUrls: [ShabuImage], tags: ["Corgi", "Dog"] },
+  { name: "Zhabu4", description: "A pig-like dog. People thinks that he's a pig, but he's actually a dog", photoUrls: [ShabuImage], tags: ["Corgi", "Pig", "Pork"] },
+  { name: "Shabu5", description: "An eating dog. 150 IQ corgi who can't even catch birds", photoUrls: [ShabuImage], tags: ["Corgi", "Smart", "ShabuHatesBirds"] },
+]
 
 
 function AdoptAnimalsPage() {
 
-
+  const animalCards = animalData.map(animal => {
     return (
-        <ThemeProvider theme={theme}>
-            <Header/>
-
-            Adopt Animals
-            <Box sx={{p:5}}>
-                <Stack 
-                spacing={5} 
-                alignItems="center"
-                justifyContent={"center"}>
-                <AnimalCard/>
-                </Stack>
-            </Box>
-
-            <Footer/>
-
-        </ThemeProvider>
+      <AnimalCard
+        name={animal.name}
+        description={animal.description}
+        photoUrl={animal.photoUrls[
+          Math.floor(Math.random() * animal.photoUrls.length)]}
+        tags={animal.tags}
+      />
     )
+  });
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+
+      <Grid container component="main"
+        alignItems="center" justifyContent="center"
+        spacing={2} p={5}>
+
+        <Grid item xs={12}>
+          <Typography variant='h5'
+            sx={{
+              textAlign: "center",
+              fontWeight: "bold"
+            }}>
+            Adopt Animals
+          </Typography></Grid>
+
+        <Grid item xs={12}>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              backgroundColor: "purple",
+              alignContent: "center",
+              justifyContent: "center"
+            }}>
+            {animalCards}
+          </div>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Footer />
+        </Grid>
+      </Grid>
+
+    </ThemeProvider>
+  )
 }
 
 export default AdoptAnimalsPage;
