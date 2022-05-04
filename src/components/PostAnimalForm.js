6daae12d966b542ab85dd007d0e318df1e2bd041
shabushabushabu <enigma14389@gmail.com>
2022-05-05 00:59:25 +0700

    add postanimal functions

---
+++ src/components/PostAnimalForm.js
@@ -0,0 +1,174 @@
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
+import Chip from '@mui/material/Chip';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import { createTheme, styled } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        background: {
+            default: "#ffebee"
+        },
+        primary: {
+            main: "#ce93d8",
+        },
+        secondary: {
+            main: '#84c887',
+        },
+        error: {
+            main: "#f6a5c0"
+        },
+        warning: {
+            main: "#ffbba7"
+        }
+    }
+});
+
+const defaultTags = [
+    'Dog',
+    'Cat',
+    'Shabu',
+    'ShabuEatsTarin',
+];
+
+const Input = styled('input')({
+    display: 'none',
+});
+
+function PostAnimalForm() {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [photoUrls, setPhotoUrls] = useState([]);
+    const [tags, setTags] = useState([]);
+
+    async function handleSubmitAnimal() {
+        if (name !== "") {
+            const response = await fetch("/api/animal", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name: name,
+                    description: description,
+                    photoUrls: [],
+                    tags: tags
+                })
+            });
+
+            const data = await response.json();
+            console.log(data)
+
+        } else {
+            console.log("no name")
+        }
+    };
+
+    return (
+        <Box
+            // display="flex"
+            component="form"
+            sx={{
+                padding: 5,
+                alignItems: 'center',
+                justifyContent: "center"
+            }}
+            noValidate
+            autoComplete="off"
+
+        >
+            <Grid item xs={12}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Animal Name"
+                    variant="standard"
+                    name="name"
+                    value={name}
+                    autoFocus
+                    onChange={(event) => setName(event.target.value)}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="description"
+                    label="Add Animal Descriptions"
+                    variant="standard"
+                    name="name"
+                    value={description}
+                    multiline
+                    onChange={(event) => setDescription(event.target.value)}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <Autocomplete
+                    multiple
+                    id="tags-filled"
+                    options={defaultTags}
+                    freeSolo
+                    value={tags}
+                    onChange={(event, newValue) => {
+                        setTags([
+                            ...newValue,
+                        ]);
+                    }}
+                    renderTags={(value, getTagProps) =>
+                        value.map((option, index) => (
+                            <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                        ))
+                    }
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            variant="standard"
+                            label="Tags"
+                            placeholder="Add tags"
+                        />
+                    )}
+                />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 3, mb: 2 }}>
+                <Stack direction="row" alignItems="center" spacing={2}>
+                    <label htmlFor="contained-button-file">
+                        <Input accept="image/*" id="contained-button-file" multiple type="file" />
+                        <Button variant="contained" component="span">
+                            Upload Image
+                        </Button>
+                    </label>
+                    <label htmlFor="icon-button-file">
+                        <Input accept="image/*" id="icon-button-file" type="file" />
+                        <IconButton color="primary" aria-label="upload picture" component="span">
+                            <PhotoCamera />
+                        </IconButton>
+                    </label>
+                </Stack>
+            </Grid>
+            <Grid item xs={12}>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    endIcon={<SendIcon />}
+                    onClick={handleSubmitAnimal}
+                    sx={{ mt: 3, mb: 2, bgcolor: "primary.dark" }}>
+                    Submit
+                </Button>
+            </Grid>
+        </Box>
+    );
+}
+
+export default PostAnimalForm;
--- src/pages/PostAnimals.js
+++ src/pages/PostAnimals.js
@@ -1,50 +1,76 @@
-import react, {Component, useState, useEffect} from 'react';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
+import { Navigate } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import PageCard from "../components/PageCard"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import PostAnimalForm from "../components/PostAnimalForm";
 
-import CorgiImage from "../figures/SHABU_profile.jpg"
+import CorgiImage from "../figures/SHABU_profile.jpg";
+
+import { isLogin } from '../functions/Authen';
 
 
 const theme = createTheme({
-    palette: {
-      background: {
-        default: "#ffebee"
-        },
-      primary: {
-        main: "#ce93d8",
-      },
-      secondary: {
-        main: '#84c887',
-      },
-      error: {
-          main: "#f6a5c0"
-      },
-      warning: {
-          main: "#ffbba7"
-      }
+  palette: {
+    background: {
+      default: "#ffebee"
+    },
+    primary: {
+      main: "#ce93d8",
+    },
+    secondary: {
+      main: '#84c887',
+    },
+    error: {
+      main: "#f6a5c0"
+    },
+    warning: {
+      main: "#ffbba7"
     }
-  });
+  }
+});
 
 
 function PostAnimalsPage() {
 
-
+  if (!isLogin()) {
     return (
-        <ThemeProvider theme={theme}>
-            <Header/>
-
-            Post Animals
-
-
-            <Footer/>
-
-        </ThemeProvider>
+      <Navigate to="/login" />
     )
+  };
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+
+      <Grid container component="main"
+        alignItems="center" justifyContent="center"
+        spacing={2} p={5}>
+
+        <Grid item xs={12}>
+          <Typography variant='h5'
+            sx={{
+              textAlign: "center",
+              fontWeight: "bold"
+            }}>
+            Post Animals
+          </Typography></Grid>
+
+        <Grid item xs={12}
+          bgcolor={"background.default"}>
+          <PostAnimalForm />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Footer />
+        </Grid>
+      </Grid>
+
+    </ThemeProvider>
+  )
 }
 
 export default PostAnimalsPage;
